4e1bb7c8bf43c110656efd32133102a923be6b9a
lorelles <74384017+lorelles@users.noreply.github.com>
2021-01-08 16:30:26 -0800

    update

---
+++ PyBank/main.py/financial.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+# Path to collect data from the resources folder
+csv_file = os.path.join("..", "resources", "02-Homework_03-Python_PyBank_Resources_budget_data (1).csv")
+
+# Define function to return number of months included in dataset
+
+# Define function to calculate net total amount of "Profit/Losses" over entire period
+
+# Define function to calculate changes in "Profit/Losses" over entie period
+# Then find average of changes in "Profit/Losses"
+
+# Define function to calculate greatest increase in profits (Date and amount) over entire period
+
+# Define function to calculate greatest decrease in losses (Date and amount) over entire period
+
+
+## Script shoud print analysis to the terminal and export text file with results
