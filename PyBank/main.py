e66f4d9f5c94d25a5b5f9ee9f7dcdb3dc03e9e99
lorelles <74384017+lorelles@users.noreply.github.com>
2021-02-08 13:38:06 -0800

    Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@ financial_csv = os.path.join("resources", "budget_data.csv")
 total_months = []
 net_amount = []
 total_change = []
-#mean_change = []
+change_list = []
 #max_increase = []
 #max_decrease = []
 
@@ -20,7 +20,7 @@ with open(financial_csv, newline='') as csv_file:
     #print(f"Header: {csv_header}")    
     
     #count = 0
-
+    
     # Loop through data
     for row in csv_reader:
         ##date = string(financial_data[0])
@@ -35,24 +35,15 @@ with open(financial_csv, newline='') as csv_file:
         net_amount.append(int(row[1]))
         
         # Calculate change in profit/loss
-        total_change.append(int(row[1]))
+        total_change.append(row)
 
-
-
-    
-        # Add greatest increase change
-        #max_change.append(int(row[1]))
-        # Add greatest decrease in change
-        #max_decrease.append(int(row[1]
-
-        # Define function to calculate changes in "Profit/Losses" over entie period
-        #total_change = sum(total_change)
-
-        # Then find average of changes in "Profit/Losses"
-        ##mean_change = total_change.mean
-
-        # Define function to calculate greatest increase in profits (Date and amount) over entire period
-        ##max_increase = net_amount.max
+    #Calculate the monthly change in profit/loss
+    ##Formula to calculate this value was obtained from Charlie Loveall who received the formula from his tutor.
+    for idx, value in enumerate(total_change):
+        if idx == 85:
+            break
+        change= int(total_change[idx+1][1])-int(total_change[idx][1])
+        change_list.append(change)
 
 #Determine total amount of months listed in data
 total_mons = len(total_months) 
@@ -60,12 +51,15 @@ total_mons = len(total_months)
 # Calculate net total amount of "Profit/Losses" over entire period
 total_amount = sum(net_amount)
 
-#print(total_change)
-# Define function to calculate greatest decrease in losses (Date and amount) over entire period
-# #max_decrease = net_amount.min
+#Calculate average change in profit/losses over entire period
+average_change = round(mean(change_list),2)
 
 # Define function to calculate greatest increase in profits (Date and amount) over entire period
-##max_increase = net_amount.max
+max_increase = round(max(change_list),2)
+
+#print(total_change)
+# Define function to calculate greatest decrease in losses (Date and amount) over entire period
+max_decrease = round(min(change_list),2)
 
 # Find total number of months in data set
 #total_mons = len(total_months)
@@ -78,8 +72,8 @@ print("Financial Analysis")
 print("-------------------")
 print(f"Total Months: {total_mons}")
 print(f"Total: ${total_amount}")
-#print(f"Average Change:  #{}")
-#print(f"Greatest Increase in Profits: ${}")
-#print(f"Greatest Decrease in Profits: ${}")
+print(f"Average Change:  ${average_change}")
+print(f"Greatest Increase in Profits: ${max_increase}")
+print(f"Greatest Decrease in Profits: ${max_decrease}")
 
 output_file = os.path.join("PyBank.txt")
